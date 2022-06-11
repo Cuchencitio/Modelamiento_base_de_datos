93a14db97fa8f82c2a675dccd72cee3f97b0decc
Cuchencitio <cesarmaureira95@gmail.com>
2022-06-11 13:39:36 +0000

    Se modifica tabla, se agregn PK Y FK

--- parque.sql
+++ parque.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Cliente ( 
+CREATE TABLE Clientes ( 
     rut_cliente NUMBER(11) Not Null, 
     dv CHAR(1) NOT NULL, 
     primer_nombre VARCHAR2(250) NOT NULL, 
@@ -11,12 +11,12 @@ CREATE TABLE Cliente (
     comuna_id NUMBER NOT NULL, 
 );
 
-ALTER TABLE Cliente ADD CONSTRAINT cliente_pk PRIMARY KEY (rut_cliente)
+ALTER TABLE Clientes ADD CONSTRAINT cliente_pk PRIMARY KEY (rut_cliente)
 
-CREATE TABLE Comuna (
+CREATE TABLE Comunas (
     id_comuna NUMBER NOT NULL,
     nombre VARCHAR2(250) NOT NULL,
     provincia_id NUMBER NOT NULL
 );
 
-ALTER TABLE Cliente ADD CONSTRAINT comuna_pk PRIMARY KEY (id_comuna)
+ALTER TABLE Comunas ADD CONSTRAINT comuna_pk PRIMARY KEY (id_comuna)
